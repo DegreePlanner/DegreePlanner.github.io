caf88714a69becab9db9a5a0ad60b26b5bcfd6ff
Adamnathaniel <adamngoldstein@gmail.com>
2022-12-04 10:41:36 -0500

    adds some previously taken courses to the user

--- userData.js
+++ userData.js
@@ -4,5 +4,12 @@ userData = [
         "pass": "SamplePass",
         "email": "Sample@domain.com",
         "major": "Computer Science General Track",
+        "takenCourses": [
+            "CSC110",
+            "CMP120",
+            "MUS120",
+            "MTH210",
+            "CSC140",
+        ]
     }
 ];
